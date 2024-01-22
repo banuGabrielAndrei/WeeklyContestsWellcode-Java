e39b3145635321356f745f5160c26a74a60b9b65
banuGabrielAndrei <banu.gabrielandrei@gmail.com>
2024-01-22 23:00:51 +0200

    Identation fixed

--- src/week87/Problem2.java
+++ src/week87/Problem2.java
@@ -7,7 +7,7 @@ public class Problem2 {
   public static void main(String[] args) {
     var scanner = new Scanner(System.in);
     Integer[] v = new Integer[21];
-	Integer[] w = new Integer[21];
+    Integer[] w = new Integer[21];
     int index = 0, indexW = 0;
     while (true) {
       int nr = scanner.nextInt();
@@ -17,37 +17,37 @@ public class Problem2 {
       v[index] = nr;
       ++index;
     }
-	int[] fr = new int[10];
+    int[] fr = new int[10];
     for (int i = 0; i < index; ++i) {
-		boolean isValid = true;
-		int number = v[i];
+      boolean isValid = true;
+      int number = v[i];
       while (number != 0) {
-		  if (number % 10 == 0) {
-			  isValid = false;
-		  }
-		  number /= 10;
+        if (number % 10 == 0) {
+          isValid = false;
+        }
+        number /= 10;
       }
-	  if (isValid) {
-		  w[indexW] = v[i];
-		  ++indexW;
-	  }
+      if (isValid) {
+        w[indexW] = v[i];
+        ++indexW;
+      }
     }
-	for (int j = 0 ; j < indexW; ++j) {
-        while (w[j] != 0) {
-			++fr[w[j] % 10];
-			w[j] /= 10;
+    for (int j = 0; j < indexW; ++j) {
+      while (w[j] != 0) {
+        ++fr[w[j] % 10];
+        w[j] /= 10;
+      }
+    }
+    int max = 7;
+    int result = 0;
+    for (int k = 0; k < 10; ++k) {
+      if (fr[k] > 0) {
+        if (fr[k] <= max) {
+          max = fr[k];
+          result = k;
         }
-	}
-	int max = 7;
-	int result = 0;
-	for (int k = 0; k < 10; ++k) {
-        if (fr[k] > 0) {
-			if (fr[k] <= max) {
-				max = fr[k];
-				result = k;
-			}
-        }
-	}
+      }
+    }
     System.out.println(result);
   }
 }
